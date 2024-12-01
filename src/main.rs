b9d3b411da165dee2f32826acc088e877cde057c
rift <117926989+Riftriot@users.noreply.github.com>
2024-12-01 17:38:24 -0600

    idk why i did that

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use hmac::Hmac;
-use pbkdf2::pbkdf2;
+use pbkdf2::{pbkdf2, pbkdf2_hmac_array};
 use sha1::Sha1;
 use base64::prelude::*;
 use indicatif::ProgressBar;
@@ -24,7 +24,6 @@ fn main() {
     let mut salt = BASE64_STANDARD.decode(args.salt);
 
     let mut passcode = vec![0; 4];
-    let mut out = vec![0u8; 20]; // hmac output buffer, the output is 20 long
 
     let bar = ProgressBar::new(9999);
     println!("Cracking screen time hash... \n");
@@ -34,7 +33,7 @@ fn main() {
         let passcode_str = format!("{:04}", code);
         passcode.copy_from_slice(passcode_str.as_bytes());
         // i hate cryptography
-        let _ = pbkdf2::<Hmac<Sha1>>(&passcode, &salt.as_mut().unwrap(), 1000, &mut out);
+        let out = pbkdf2_hmac_array::<Sha1, 20>(&passcode, &salt.as_mut().unwrap(), 1000);
 
         if BASE64_STANDARD.encode(&out) == hash {
             bar.finish();
